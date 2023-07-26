7c885c97fef0921631f8d63bd4bc7d2d0259a3c8
Yujin Choi <99077953+choichoijin@users.noreply.github.com>
2023-07-26 13:13:42 +0900

    test commit

--- 최유진/test.java
+++ 최유진/test.java
@@ -1,1 +1,15 @@
-dddddfffdfdf
+package movie;
+
+public interface IMovieManager {
+	void add(Movie movie);
+	
+	Movie[] getList();
+	
+	Movie[] searchByTitle(String title);
+	
+	Movie[] getMovies();
+	
+	SeriesMovie[] getSeriesMovie();
+	
+	double getRunningTimeAvg();
+}
