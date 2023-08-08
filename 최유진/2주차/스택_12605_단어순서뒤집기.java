d331232ef07d90c1385b92c77ba063d4d2844ec8
Yujin Choi <99077953+choichoijin@users.noreply.github.com>
2023-08-08 17:10:46 +0900

    Update 스택_12605_단어순서뒤집기.java

--- 최유진/2주차/스택_12605_단어순서뒤집기.java
+++ 최유진/2주차/스택_12605_단어순서뒤집기.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Stack;
 
 public class bj12605 {
 
@@ -19,12 +20,26 @@ public class bj12605 {
 			StringBuilder sb = new StringBuilder();
 			String[] strArr = br.readLine().split(" ");
 			
-			//역순으로 stringBuilder에 append
-			for(int j = strArr.length-1; j > -1; j--) {
-				sb.append(strArr[j] + " ");
+			//역순으로 stringBuilder에 append (스택 미사용)
+			// for(int j = strArr.length-1; j > -1; j--) {
+			// 	sb.append(strArr[j] + " ");
+			// }
+			
+			// bw.write("Case #" + i +": " + sb);
+			// bw.write("\n");
+
+			//스택 사용
+			Stack<String> st = new Stack<>();
+			
+			for(int j = 0; j < strArr.length; j++) {
+				st.push(strArr[j]);
 			}
 			
-			bw.write("Case #" + i +": " + sb);
+			for(int m = 0; m < strArr.length; m++) {
+				sb.append(st.pop() + " ");
+			}
+			
+			bw.write("Case #" + i + ": " + sb);
 			bw.write("\n");
 		}
 		
