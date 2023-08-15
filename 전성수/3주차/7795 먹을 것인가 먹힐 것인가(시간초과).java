4fecaca6d0e2c3e177ad0bae746f647a4b830ffa
no-merit <128347576+no-merit@users.noreply.github.com>
2023-08-15 20:14:39 +0900

    Update 7795 먹을 것인가 먹힐 것인가(시간초과).java

--- 전성수/3주차/7795 먹을 것인가 먹힐 것인가(시간초과).java
+++ 전성수/3주차/7795 먹을 것인가 먹힐 것인가(시간초과).java
@@ -35,6 +35,8 @@ public class Main {
 			}
 			//String을 int로 바꿔줌
 			int cnt =0; //쌍의 개수
+			//A_num 과 B_num을 정렬한 뒤 반복문에 A_num[i] <= B_num[j] 인 경우 
+			//어차피 뒤의 것듫 전부 A_num보다 클 것이기 떄문에 break를 넣으면 시간초과가 안남
 			
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<M; j++) {
