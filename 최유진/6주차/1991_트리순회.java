5d1c0ea91793b74927bae2e6be87d79567aa2fed
Yujin Choi <99077953+choichoijin@users.noreply.github.com>
2023-09-03 16:29:33 +0900

    Update 1991_트리순회.java

--- 최유진/6주차/1991_트리순회.java
+++ 최유진/6주차/1991_트리순회.java
@@ -22,6 +22,9 @@ public class bj1991 {
 			child[i][1] = line[2];
 		}
 
+		//tree = {A, B, C, E, F, D, G}  -> 노드
+		//child = {{B, C}, {D, .}, {E, F}, {., .}, {., G}, {., .}, {., .} -> 각 노드가 가진 자식
+
 		preorder(0);
 		System.out.println("");
 		order(0);
@@ -34,9 +37,9 @@ public class bj1991 {
     
 		if (!child[i][0].equals(".")) { // 왼쪽 자식이 있다면.
 			for(int j = 0; j < N; j++) {
-				if(tree[j].equals(child[i][0])) { //인덱스르 돌면서 자식 원소에 해당하는 정보를 찾습니다.
-          // Q. i * 2 가 자식 노드인데 굳이 for문 안 돌고 하면 안 되나요?
-          // A. 문제의 입력에서 A, B, C, D .. 순서로 주고 있지 않습니다. 따라서 인덱스에 *2 했더니 원하는 값이 나오지 않았어요!
+				if(tree[j].equals(child[i][0])) { //인덱스를 돌면서 자식 원소에 해당하는 정보를 찾습니다.
+         				 // Q. i * 2 가 자식 노드인데 굳이 for문 안 돌고 하면 안 되나요?
+          				// A. 문제의 입력에서 A, B, C, D .. 순서로 주고 있지 않습니다. 따라서 인덱스에 *2 했더니 원하는 값이 나오지 않았어요!
 					preorder(j);
 				}
 			}
