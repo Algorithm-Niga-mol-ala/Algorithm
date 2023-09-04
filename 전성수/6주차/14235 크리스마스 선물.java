25e44e975d5fbcec0fe58853f3e434b4be5a1e54
no-merit <128347576+no-merit@users.noreply.github.com>
2023-09-04 10:08:22 +0900

    Create 14235 크리스마스 선물.java

---
+++ 전성수/6주차/14235 크리스마스 선물.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+	// 아이들과 거점지를 방문한 횟수 n
+	// 다음 n 줄에는 a 가 들어오고 그 다음 a개의 숫자
+	// 거점지에서 a개의 선물을 충전하는 것이고. 숫자들은 선물의 가치
+	// 만약 a가 0이면 거점지가 아니라 아이임
+	// 선물의 가치는 100,000보다 작은 양의 정수
+
+  // 내림차순으로 만들어서 출력해주는 문제
+  
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		// 아이들과 거점지를 방문한 횟수
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+
+				return o2 - o1;
+			}//내림차순으로 
+
+		});
+		
+		
+		int cnt = 0; // 산타가 가지고 있는 선물의 개수
+		
+		for (int i = 0; i < n; i++) {//a 받기
+			String[] a_str = br.readLine().split(" ");
+			int a = Integer.parseInt(a_str[0]);
+      
+			switch (a) {
+          //a 가 0인 경우
+			case 0:
+				if (pq.isEmpty()) {
+					sb.append(String.valueOf(-1)).append("\n");
+					break;
+				}
+				sb.append(pq.poll()).append("\n");
+				break;
+
+          //a 가 0이 아닌 모든 경우
+			default:
+				int idx = 1;
+          
+				while (idx<a_str.length) {//a의 길이가 입력마다 바뀌므로 범위에 신경써야함
+					pq.add(Integer.parseInt(a_str[idx++]));
+					cnt++;
+				}
+				break;
+			}
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
