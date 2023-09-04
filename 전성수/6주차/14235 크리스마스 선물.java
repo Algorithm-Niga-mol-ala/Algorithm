15070ed2a753da18438eddc565f432870d6010f8
no-merit <128347576+no-merit@users.noreply.github.com>
2023-09-04 10:15:18 +0900

    Update 14235 크리스마스 선물.java

--- 전성수/6주차/14235 크리스마스 선물.java
+++ 전성수/6주차/14235 크리스마스 선물.java
@@ -6,13 +6,19 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
-	// 아이들과 거점지를 방문한 횟수 n
-	// 다음 n 줄에는 a 가 들어오고 그 다음 a개의 숫자
+	// 아이들과 거점지를 방문한 횟수 (1≤n≤5,000)
+	
+	// 다음 n 줄에는 a 가 들어오고 그 다음 a개의 숫자(1≤a≤100)
+	
 	// 거점지에서 a개의 선물을 충전하는 것이고. 숫자들은 선물의 가치
+	
 	// 만약 a가 0이면 거점지가 아니라 아이임
+	
 	// 선물의 가치는 100,000보다 작은 양의 정수
-
-  // 내림차순으로 만들어서 출력해주는 문제
+	
+	// a가 100까지니까 한번의 입력에 100개의 숫자가 들어올 수 있고 이거 5000번까지 가능하니까 최대 500,000개의 숫자가 배열에 들어가니까 n^2의 정렬을 이용하면 시간초과
+       
+	// 내림차순으로 만들어서 출력해주는 문제
   
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
