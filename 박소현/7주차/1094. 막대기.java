bb1b2259ed5528a6a47f8dbaf82150595022f5bd
sohoneyee <139411346+sohoneyee@users.noreply.github.com>
2023-09-09 12:50:19 +0900

    Update 1094. 막대기.java
    0909 sol

--- 박소현/7주차/1094. 막대기.java
+++ 박소현/7주차/1094. 막대기.java
@@ -3,19 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main1094_막대기 {
-
-	static int X, stick;
-	static int ans;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		X = Integer.parseInt(br.readLine());
+		int X = Integer.parseInt(br.readLine());
 		
 		// 23 = 10111, 32 = 100000, 64 = 1000000, 48 = 110000
 		// X를 2진수로 변환했을 때 1의 개수를 세주면 됨!!!! 이번엔 찐 ㅎㅎ
 		// tobinaryString(2), toOctalString(8), toHexString(16)
 		
 		// 문자열로 변환되기 때문에 split으롭 배열에 담아주고 1의 개수를 세줌
+		int ans =0;
 		String[] two = Integer.toBinaryString(X).split("");
 		for(int i =0; i<two.length; i++) {
 			if (two[i].equals("1")) {
