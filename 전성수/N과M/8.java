ba08f0073757492e410a1d910d7adfcbfc7bcf1d
no-merit <128347576+no-merit@users.noreply.github.com>
2023-09-24 12:56:45 +0900

    N&M 8.java

---
+++ 전성수/N과M/8.java
@@ -0,0 +1,62 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+	static StringBuilder sb;
+	static int N,M;
+	static boolean[] visited;
+	static int[] ans, nums;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+		
+		String [] NM = br.readLine().split(" ");
+		
+		N = Integer.parseInt(NM[0]);	//1부터 N까지의 자연수
+		M= Integer.parseInt(NM[1]); 	//M개 선택한 수열
+		visited = new boolean[N+1];
+		ans = new int[M];
+		String [] Ns = br.readLine().split(" ");
+		nums = new int [N];
+		for(int i=0; i<N; i++) {
+			nums[i] = Integer.parseInt(Ns[i]);
+		}
+		Arrays.sort(nums);
+		
+		dfs(0,0);
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	private static void dfs(int depth,int st) {
+		if(depth==M) {
+			for(int i=0; i<M; i++) {
+				
+				sb.append(ans[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=st; i<N; i++) {
+			
+			
+			ans[depth]=nums[i];
+		
+			dfs(depth+1,i);
+		
+		
+			
+		}
+	}
+
+}
