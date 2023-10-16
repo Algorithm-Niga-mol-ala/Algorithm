2a6cdf83b9145ebe75f6989dfa18e07262b65dc5
yeyeah1 <139411481+yeyeah1@users.noreply.github.com>
2023-10-17 00:22:13 +0900

    Create 14425. 문자열 집합.java

---
+++ 이예원/11주차/14425. 문자열 집합.java
@@ -0,0 +1,36 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		HashSet<String> set = new HashSet<>();
+
+		for (int i = 0; i < N; i++) {
+			set.add(br.readLine());
+		} // 먼저 들어오는 값 set에 저장
+
+		int cnt = 0;
+
+		for (int i = 0; i < M; i++) {
+
+			if (set.contains(br.readLine()))
+				cnt++;
+
+		}
+		System.out.println(cnt);
+
+	}// main
+
+}
