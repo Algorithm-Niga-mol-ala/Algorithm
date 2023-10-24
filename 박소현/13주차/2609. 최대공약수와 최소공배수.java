60fddb5c4674b970874ea1c7f90e83dcf0f5bb8e
sohoneyee <139411346+sohoneyee@users.noreply.github.com>
2023-10-25 00:07:54 +0900

    Create 2609. 최대공약수와 최소공배수.java
    231024 sol

---
+++ 박소현/13주차/2609. 최대공약수와 최소공배수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main2609_최대공약수와최소공배수 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+
+		int gcd = 0; // 최대공약수
+		int lcm = a; // 최소공배수, 최소공배수는 a나 b를 약수로 함(필수)
+		for (int i = Math.min(a, b); i >= 1; i--) {
+			if (a % i == 0 && b % i == 0) {
+				gcd = i;
+				break;
+			}
+		}
+
+		// a*b = lcm*gcd -> lcm = a*(b/gcd)
+		lcm *= b / gcd;
+
+		System.out.println(gcd);
+		System.out.println(lcm);
+	} // main
+}
