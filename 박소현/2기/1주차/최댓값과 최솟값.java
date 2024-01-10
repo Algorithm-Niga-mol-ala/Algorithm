21ad598c34652852568c78094b0ac6d928679228
sohoneyee <139411346+sohoneyee@users.noreply.github.com>
2024-01-11 08:56:05 +0900

    Update and rename 최댓값과 최솟값 to 최댓값과 최솟값.java

--- 박소현/2기/1주차/최댓값과 최솟값.java
+++ 박소현/2기/1주차/최댓값과 최솟값.java
@@ -4,15 +4,16 @@ import java.io.*;
 class Solution {
     public String solution(String s){
         String[] arr = s.split(" ");
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE; // 최댓값
+        int min = Integer.MAX_VALUE; // 최솟값
         
         for(int i=0; i<arr.length; i++) {
             int num = Integer.parseInt(arr[i]);
             max = Math.max(max, num);
             min = Math.min(min, num);
         }
-        
+
+        // 문자열이므로 +로 합치기
         String answer = "";
         answer+=min;
         answer+=" ";
