d318ccde23bc750581b74161afb10a3ee517df14
yangsungjoo <139411441+yangsungjoo@users.noreply.github.com>
2024-01-18 22:57:05 +0900

    Create 푸드 파이트 대회.java

---
+++ 양성주/2기/2주차/푸드 파이트 대회.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        for(int i=1;i<food.length;i++){
+            food[i] = food[i]/2;
+        }
+        for(int i=1;i<food.length;i++){
+            for(int j=0;j<food[i];j++){
+                answer += i;
+            }
+        }
+        answer += 0;
+        for(int i=answer.length()-2;i>=0;i--){
+            answer+=answer.charAt(i);
+        }
+        return answer;
+    }
+}
