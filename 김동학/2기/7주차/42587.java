76097ecde316ce8778bc82f8693035345fc4ed8e
donghakk <132239086+donghakk@users.noreply.github.com>
2024-03-05 09:34:33 +0900

    Update 42587.java

--- 김동학/2기/7주차/42587.java
+++ 김동학/2기/7주차/42587.java
@@ -1,18 +1,28 @@
-import java.util.LinkedList; 
-import java.util.Queue; 
+import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<Integer> que = new LinkedList<>();
-        int answer = 0; // count
-        // peek add poll
-        for(int i = 0 ; i<priorities.length; i++){
-            que.add(priorities[i]);        
+        int answer = 0;
+        Queue<Integer> que = new LinkedList<Integer>();
+        for(int p : priorities)
+            que.add(p);
+        Arrays.sort(priorities);
+        int len = priorities.length-1;
+        // priorites의 0인덱스부터 바라보면서 que의 프로세스 값을 비교해본다.
+        while(!que.isEmpty()){
+            int peek = que.poll();
+            if(peek == priorities[len - answer]){ // 실행해야할 우선순위의 프로세스
+                answer++;
+                location--;
+                if(location<0)
+                    break;
+            }
+            else{ // 실행할 우선순위가 아닌 프로세스
+                que.add(peek);
+                location--;
+                if(location<0) location = que.size()-1;
+            }
         }
-        for(int i = 0 ; i<priorities.length; i++){
-            que.poll(priorities[i]);        
-        }
-        
         return answer;
     }
 }
