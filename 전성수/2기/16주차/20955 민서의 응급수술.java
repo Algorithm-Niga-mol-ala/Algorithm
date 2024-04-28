750deff709bb17d22a169986dd52a9680b6dfb4e
no-merit <128347576+no-merit@users.noreply.github.com>
2024-04-29 00:11:00 +0900

    Update 20955 민서의 응급수술.java

--- 전성수/2기/16주차/20955 민서의 응급수술.java
+++ 전성수/2기/16주차/20955 민서의 응급수술.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.LinkedList; 
 import java.util.Queue;
 
 public class Main {
